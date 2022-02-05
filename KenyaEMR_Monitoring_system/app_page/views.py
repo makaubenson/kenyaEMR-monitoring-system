15352d0d657d517a2b3ab1e2b7bbbea8f85c3e02
kagwe <kagwepeter07@gmail.com>
2022-02-05 10:45:00 +0300

    'updated'

--- KenyaEMR_Monitoring_system/app_page/views.py
+++ KenyaEMR_Monitoring_system/app_page/views.py
@@ -7,7 +7,11 @@ import nmap, socket
 from django.http import HttpResponse
 import datetime
 from django.contrib import messages
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 checks = []
+
 def home(request):
     if request.method == 'POST':
         hostname = request.POST.get('address')
@@ -25,25 +29,38 @@ def home(request):
                 pingstatus = "Network Error"
                 print(pingstatus)
                 checks.append(pingstatus)
-                
-                
-        """ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            result = sock.connect_ex(('irc.myserver.net', 6667))
-            if result == 0:
-                portstatus = "Port is open"
-                print(portstatus)
-                checks.append(portstatus)
-            else:
-                portstatus = "Port is not open"
-                print(portstatus)
-                checks.append(portstatus)
+                pingstatus = "Network Error"
+                print(pingstatus)
+                mail_content = 'Server is Down'
+                #The mail addresses and password
+                sender_address = 'pkagwe07@gmail.com'
+                sender_pass = 'kagwepeter07@gmail.com'
+                receiver_address = 'towuor@gokhanmasterspacejv.co.ke'
+                #Setup the MIME
+                message = MIMEMultipart()
+                message['From'] = sender_address
+                message['To'] = receiver_address
+                message['Subject'] = 'Alert!'   #The subject line
+                #The body and the attachments for the mail
+                message.attach(MIMEText(mail_content, 'plain'))
+                #Create SMTP session for sending the mail
+                session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+                session.starttls() #enable security
+                session.login(sender_address, sender_pass) #login with mail_id and password
+                text = message.as_string()
+                session.sendmail(sender_address, receiver_address, text)
+                session.quit()
+                print('Mail Sent'
             now = datetime.datetime.now()
             html = "<html><body>It is now %s.</body></html>" % now
-            return HttpResponse(html) """
+            return HttpResponse(html)      
+
         schedule.every(0.05).minutes.do(func)
-        
         while True:
-            schedule.run_pending()
-            time.sleep(0.05)
+           schedule.run_pending()
+           time.sleep(0.05)
 
     return render(request, 'app_page/home.html')
+
+
+
